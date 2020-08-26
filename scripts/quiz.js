b615710ef7df6fdbf3e20ecdfc452aa88fa6e1f3
sashulka1 <31404293+sashulka1@users.noreply.github.com>
2020-08-26 15:37:33 +0200

    1.7
    got the quiz working, quiz questions done, quiz nees formating

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,5 +1,5 @@
 function check(){
-
+    var correct = 0;
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
@@ -10,29 +10,30 @@ function check(){
     var question8 = document.quiz.question8.value;
 
     if (question1 == "true") {
-    correct =+ 1;
+    correct = correct + 1;
     }
     if (question2 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question3 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question4 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question5 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question6 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question7 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
     if (question8 == "true") {
-        correct =+ 1;
+        correct = correct + 1;
     }
-    document.getElementById("after_submit").style.visibility = "visible";
-    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+    document.getElementById("quiz").style.visibility = "hidden";
+    document.getElementById("score").style.visibility = "visible";
+    document.getElementById("score").innerHTML = "You got " + correct + " correct.";
 }
